df1920ea0e80568c3fcf2b35fd6451ee49da9584
Erosdd2 <eros.santos@escolar.ifrn.edu.br>
2026-09-15 14:27:52 -0300

    exemplos aula 17

---
+++ listas/exemplos_aula17.py
@@ -0,0 +1,45 @@
+#exemplo 1
+
+# lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+# print(lanche)
+
+#exemplo 2
+
+# lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+# lanche[3] = 'picolé '
+
+#exemplo 3
+"""
+lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+lanche.append('cookie')
+print(lanche)   
+
+ lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+ lanche.insert(0,'cachorro quente')
+print(lanche)
+
+
+
+lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+lanche.append('cookie')
+lanche.insert(0,'cachorro quente')
+print(lanche)
+
+
+
+lanche = ['hamburguer', 'suco', 'pizza', 'pudim']
+lanche.append('cookie')
+lanche.insert(0,'cachorro quente')
+lanche.remove('suco')
+if 'pizza' in lanche:
+    lanche.remove('pizza')
+print(lanche)
+
+
+valores = list(range(4,11))
+print(valores)
+
+valores = [8, 2, 5, 4, 9, 3, 0]
+valores.sort()
+print(valores)
+"""
